2fbfa11db3ad0979d59b032496ae31cb410d9378
Abhishek Dawer <abhishek.dawer@metacube.com>
2021-02-25 19:04:41 +0530

    adding SCFO assignment

---
+++ GET 2021/src/SCFO/ShoppingCart.java
@@ -0,0 +1,15 @@
+package SCFO;
+
+import java.util.HashMap;
+
+public class ShoppingCart {
+ 
+	HashMap<String,Integer> hm1 = new HashMap<>();
+	HashMap<String,Integer> hm2 = new HashMap<>();
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
