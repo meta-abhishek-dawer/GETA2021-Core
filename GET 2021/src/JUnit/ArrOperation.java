7772af9265419302d8f48f94047147eb101bb44e
Abhishek Dawer <abhishek.dawer@metacube.com>
2021-02-28 22:53:50 +0530

    adding method of fixXY problem and its junit testing

--- GET 2021/src/JUnit/ArrOperation.java
+++ GET 2021/src/JUnit/ArrOperation.java
@@ -3,10 +3,15 @@ package JUnit;
 import java.util.Scanner;
 
 public class ArrOperation {
+	/**
+	 * Find largest Mirror Section in array
+	 * @param arr contain values in which we find mirror section
+	 * @return maxResult as size of largest mirror section in array
+	 */
 	public int findLargestMirror(int[] arr){
 		int maxResult=0;
 		try{
-			if(arr.length==0)
+			if(arr.length==0) // if no element is present in array then throw error
 				throw new AssertionError();
 			
 			for(int i=0;i<arr.length;i++){
@@ -27,6 +32,11 @@ public class ArrOperation {
 		}
 		return maxResult;
 	}
+	/**
+	 * Find number of Clumps in array
+	 * @param arr contain values in which we find series of 2 or more adjacant elements
+	 * @return numberOfClumps present in array
+	 */
 	public int findNumberClumps(int[] arr){
 		int numberOfClumps =0;
 		try{
@@ -47,12 +57,17 @@ public class ArrOperation {
 		}
 		return numberOfClumps;
 	}
+	/**
+	 * Find index where left sum of array is equal to right sum of array
+	 * @param arr contain values where we find index
+	 * @return index of splitArray
+	 */
 	public int findIndexSplitArray(int[] arr){
 		int leftSum =0;
 		for(int i=0;i<arr.length;i++){
 			leftSum += arr[i];
-			int rightSum =0;
 			
+			int rightSum =0;
 			for(int j= i+1;j<arr.length;j++)
 				rightSum += arr[j];
 			
@@ -60,6 +75,52 @@ public class ArrOperation {
 				return i+1;
 		}
 		return -1;
+	}
+	/**
+	 * Solve fixXY problem in array
+	 * @param arr contain values which we have to rearrange
+	 * @param x is value which do not move
+	 * @param y is value which we place after x
+	 * @return arr array after arranging x and y
+	 */
+	public int[] fixXY(int[] arr, int x, int y){
+		try{
+			if(arr.length==0)
+				throw new AssertionError();
+			else if(arr[arr.length-1]==x)
+				throw new AssertionError();
+			else{
+				for(int i=0;i<arr.length-1;i++){
+					if(arr[i]==x && arr[i+1]==x)
+						throw new AssertionError();
+				}
+				int numberOfX=0, numberOfY=0;
+				for(int i=0;i<arr.length;i++){
+					if(arr[i]==x)
+						numberOfX++;
+					else if(arr[i]==y)
+						numberOfY++;
+				}
+				if(numberOfX!= numberOfY)
+					throw new AssertionError();
+				
+				for(int i=0;i<arr.length;i++){
+					if(arr[i]==y){
+						for(int j=1;j<arr.length;j++){
+							if(arr[j]==x && arr[j+1]!=y && j!=arr.length-1){
+								int temp = arr[i];
+								arr[i] = arr[j+1];
+								arr[j+1] = temp;
+							}
+						}
+					}
+				}
+			}
+		}
+		catch(AssertionError e){
+			System.out.println("Invalid Input!!");
+		}
+		return arr;
 	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -96,6 +157,22 @@ public class ArrOperation {
 		ArrOperation newArrOperation3 = new ArrOperation();
 		int indexResult = newArrOperation3.findIndexSplitArray(arr);
 		System.out.println("index where left split sum = right split sum:- "+ indexResult);
+		
+		//method to solve fixXY problem
+		System.out.println("enter array size and elements to find index in array:- ");
+		int numberOfElements4 = scanner.nextInt();
+		int[] arr4 = new int[numberOfElements4];
+		for(int i=0;i<numberOfElements4;i++)
+			arr4[i] = scanner.nextInt();
+		
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		ArrOperation newArrOperation4 = new ArrOperation();
+		int[] resultArr = newArrOperation4.fixXY(arr4, x, y);
+		System.out.println("after fixing array:- ");
+		
+		for(int i=0;i<resultArr.length;i++)
+			System.out.print(resultArr[i]+" ");
 
 	}
 
