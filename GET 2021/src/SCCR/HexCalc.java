4ec2101ad8aced6caecfcdb7800e4ed66372a233
Abhishek Dawer <abhishek.dawer@metacube.com>
2021-03-02 16:56:01 +0530

    changes done as per reviews

--- GET 2021/src/SCCR/HexCalc.java
+++ GET 2021/src/SCCR/HexCalc.java
@@ -3,100 +3,109 @@ package SCCR;
 import java.util.Scanner;
 public class HexCalc {
 	
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		String hexNum1 = sc.next();
-		String hexNum2 = sc.next();  
-		HexCalc obj1 = new HexCalc();
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.println("enter two hexadecimal numbers.");
+	    String hexadecimalNumber1 = scanner.next();
+        String hexadecimalNumber2 = scanner.next();  
+	    HexCalc hexCalculator = new HexCalc();
+		      
+	    //method for adding two hexadecimal numbers
+	    String addResult = hexCalculator.add(hexadecimalNumber1, hexadecimalNumber2);
+	    System.out.println(addResult);
 		   
-		//convert hexadecimal string to numbers
-		int decNum1 = obj1.HexadecimalToDecimal(hexNum1);
-		int decNum2 = obj1.HexadecimalToDecimal(hexNum2);
+	    //method for subtracting two hexadecimal numbers
+	    String subtractResult = hexCalculator.subtract(hexadecimalNumber1, hexadecimalNumber2);
+	    System.out.println(subtractResult);
 		   
-		//method for adding two hexadecimal numbers
-		int add = obj1.addTwoHexadecimals(decNum1,decNum2);
-		String sumResult= obj1.decimalToHexadecimal(add);
-		System.out.println(sumResult);
+	    //method for multiply two hexadecimal numbers
+        String multiplyResult = hexCalculator.multiply(hexadecimalNumber1, hexadecimalNumber2);
+	    System.out.println(multiplyResult);
 		   
-		//method for subtracting two hexadecimal numbers
-		int subtract = obj1.subtractTwoHexadecimals(decNum1,decNum2);
-		String subtractResult= obj1.decimalToHexadecimal(subtract);
-		System.out.println(subtractResult);
+	    //method for divide two hexadecimal numbers
+	    String divideResult = hexCalculator.divide(hexadecimalNumber1, hexadecimalNumber2);
+	    System.out.println(divideResult);
 		   
-		//method for multiply two hexadecimal numbers
-		int multiply = obj1.multiplyTwoHexadecimals(decNum1,decNum2);
-		String multiplyResult= obj1.decimalToHexadecimal(multiply);
-		System.out.println(multiplyResult);
+	    //method to check equality
+	    boolean equalResult = hexCalculator.checkEquality(hexadecimalNumber1, hexadecimalNumber2);
+	    System.out.println(equalResult);
 		   
-		//method for divide two hexadecimal numbers
-		int divide = obj1.divideTwoHexadecimals(decNum1,decNum2);
-		String divideResult= obj1.decimalToHexadecimal(divide);
-		System.out.println(divideResult);
+	    //method to check greater
+	    boolean greaterResult = hexCalculator.checkGreater(hexadecimalNumber1, hexadecimalNumber2);
+	    System.out.println(greaterResult);
 		   
-		//method to check equality
-		boolean equalResult = obj1.checkEquality(hexNum1, hexNum2);
-		System.out.println(equalResult);
-		   
-		//method to check greater
-		boolean greaterResult = obj1.checkGreater(hexNum1,hexNum2);
-		System.out.println(greaterResult);
-		   
-		//method to check smaller
-		boolean smallerResult = obj1.checkSmaller(hexNum1,hexNum2);
-		System.out.println(smallerResult);
-	}
+	    //method to check smaller
+	    boolean smallerResult = hexCalculator.checkSmaller(hexadecimalNumber1, hexadecimalNumber2);
+	    System.out.println(smallerResult);
+    }
 
-	// function to convert hexadecimal to decimal
-	public int HexadecimalToDecimal(String value1){
-		return Integer.parseInt(value1, 16);
-	}
+    // function to convert hexadecimal to decimal
+    public int hexadecimalToDecimal(String value1) {
+    	int baseValue =16;
+	    return Integer.parseInt(value1, baseValue);
+    }
 	
-	//function to convert decimal to hexadecimal
-	public String decimalToHexadecimal(int value){
-		return Integer.toHexString(value);
-	}
+    //function to convert decimal to hexadecimal
+    public String decimalToHexadecimal(int value) {
+	    return Integer.toHexString(value);
+    }
 	
-	//function to add two hexadecimals
-	public int addTwoHexadecimals(int value1,int value2){
-		return value1+value2;
-	}
+    //function to add two hexadecimals
+    public String add(String value1, String value2) {
+    	int number1 = this.hexadecimalToDecimal(value1);
+    	int number2 = this.hexadecimalToDecimal(value2);
+	    int result=  number1 + number2;
+	    return this.decimalToHexadecimal(result);
+    }
 	
-	//function to subtract two hexadecimals
-	public int subtractTwoHexadecimals(int value1,int value2){
-		return value1-value2;
-	}
+    //function to subtract two hexadecimals
+    public String subtract(String value1, String value2) {
+    	int number1 = this.hexadecimalToDecimal(value1);
+    	int number2 = this.hexadecimalToDecimal(value2);
+	    int subtractResult= number1 - number2;
+	    return this.decimalToHexadecimal(subtractResult);
+    }
 		
     //function to multiply two hexadecimals
-	public int multiplyTwoHexadecimals(int value1,int value2){
-		return value1*value2;
+	public String multiply(String value1, String value2) {
+		int number1 = this.hexadecimalToDecimal(value1);
+		int number2 = this.hexadecimalToDecimal(value2);
+		int multiplyResult= number1 * number2;
+		return this.decimalToHexadecimal(multiplyResult);
 	}
 				
-	//function to divide two hexadecimals
-	public int divideTwoHexadecimals(int value1,int value2){
-		return value1/value2;
-	}
+    //function to divide two hexadecimals
+    public String divide(String value1, String value2) {
+    	int number1 = this.hexadecimalToDecimal(value1);
+    	int number2 = this.hexadecimalToDecimal(value2);
+	    int divideResult = number1 / number2;
+	    return this.decimalToHexadecimal(divideResult);
+    }
 				
-	//check two hexadecimal numbers are equal or not
-	public boolean checkEquality(String value1,String value2){
-		if(value1.equals(value2))
-			return true;
-		else
-			return false;
-	}
+    //check two hexadecimal numbers are equal or not
+    public boolean checkEquality(String value1, String value2) {
+    	int number1 = this.hexadecimalToDecimal(value1);
+    	int number2 = this.hexadecimalToDecimal(value2);
+	    if(number1 == number2)
+		    return true;
+		return false;
+    }
 				
-	// greater function
-	public boolean checkGreater(String value1,String value2){
-		if((value1.compareTo(value2))==1)
-			return true;
-		else
-			return false;
-	}
+    // greater function
+    public boolean checkGreater(String value1, String value2) {
+    	int number1 = this.hexadecimalToDecimal(value1);
+    	int number2 = this.hexadecimalToDecimal(value2);
+	    if(number1 > number2)
+	    	return true;
+		return false;
+    }
 				
-	//smaller function 
-	public boolean checkSmaller(String value1,String value2){
-		if((value1.compareTo(value2))== -1)
-			return true;
-		else
-			return false;
-	}			
+    //smaller function 
+    public boolean checkSmaller(String value1, String value2) {
+    	int number1 = this.hexadecimalToDecimal(value1);
+    	int number2 = this.hexadecimalToDecimal(value2);
+	    if(number2> number1)
+		    return true;
+		return false;
+    }			
 }
