2233ed8ac61b0b45381204bd3610181f2482a504
Abhishek Dawer <abhishek.dawer@metacube.com>
2021-03-03 19:24:04 +0530

    adding method rotate sublist and specification

--- GET 2021/src/OverviewDataStructures/RotateSublist.java
+++ GET 2021/src/OverviewDataStructures/RotateSublist.java
@@ -27,27 +27,60 @@ public class RotateSublist {
 		    head = newNode;	
 	    }		
     }
+    /**
+     * method to rotate sublist of linkedlist
+     * @param left starting index of sublist in linkedlist
+     * @param right ending index of sublist in linkedlist
+     * @param numberOfElements value by which we have to rotate
+     */
     public void rotateSubList(int left, int right, int numberOfElements) {
     	int size = right - left + 1;
     	if(numberOfElements > size)
             numberOfElements = numberOfElements % size;
+    	if(numberOfElements == 0 || numberOfElements == size) {
+    		Node temp = head;
+    		while(temp!= null){
+    			System.out.print(temp.data);
+    			temp = temp.next;
+    		}
+    	}
+    	Node link = null;
+    	if(left == 1)
+    		link = head;
+    	Node current = head;
+    	int count =0;
+    	Node end = null;
+    	Node previous = null;
+    	while(current != null) {
+    		count++;
+    		if(count == left - 1) {
+    			previous = current;
+    			link = current.next;
+    		}
+    		if(count == right - numberOfElements) {
+    			if(left == 1) {
+    				end = current;
+    				head = current.next;
+    			}
+    			else {
+    				end = current;
+    				previous.next = current.next;
+    			}
+    		}
+    		if(count == right) {
+    			Node temp1 = current.next;
+    			current.next = link;
+    			end.next = temp1;
+    			Node head1 = head;
+    			while(head1 != null) {
+    				System.out.print(head1.data + " ");
+    				head1 = head1.next;
+    			}
+    		}
+    		current = current.next;
+    	}
     }
-    /**
-     * Method to display the list
-     */
-    public void display() {
-	    Node temp = head;
-	    if(head == null)
-		    System.out.println("LinkedList is empty!!");
-	    else {
-		    System.out.println("Elements in LinkedList:- ");
-		    while(temp!= null) {
-		        System.out.print(temp.data + " ");
-		        temp = temp.next;
-		    }
-		    System.out.println();
-	    }
-    }
+    
    
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
@@ -65,6 +98,5 @@ public class RotateSublist {
     int rightPosition = scanner.nextInt();
     int numberOfPlaces = scanner.nextInt();
     newRotateSublist.rotateSubList(leftPosition, rightPosition, numberOfPlaces);
-    newRotateSublist.display();
     }
 }
