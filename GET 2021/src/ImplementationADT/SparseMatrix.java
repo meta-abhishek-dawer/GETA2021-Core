fabecb64052e2a5369da3d7b57f94db8a7785d12
meta-abhishek-dawer <abhishek.dawer@metacube.com>
2021-03-05 15:23:29 +0530

    Adding methods and specification

--- GET 2021/src/ImplementationADT/SparseMatrix.java
+++ GET 2021/src/ImplementationADT/SparseMatrix.java
@@ -13,18 +13,31 @@ public class SparseMatrix {
         column = column1;
         length = 0;
     }
+    /**
+     * method to set size of sparse array
+     * @param size of non zero element in array
+     */
     public void setSparseLength(int size) {
     	maxElement = size;
     	sparseArr = new int[maxElement][3];
     }
+    /**
+     * Method to insert value in sparse matrix
+     * @param row1 contain position of row where non zero element is placed
+     * @param column1 contain position of column where non zero element is placed
+     * @param value contain non zero value
+     */
     public void insertInSparse(int row1, int column1, int value) {
     	sparseArr[length][0] = row1;
     	sparseArr[length][1] = column1;
     	sparseArr[length][2] = value;
     	length++;
     }
-    
-    public int[][] transposeArray() {
+    /**
+     * Method to find transpose of sparse matrix
+     * @return object of transposed sparse matrix
+     */
+    public SparseMatrix transposeMatrix() {
         SparseMatrix resultTranspose = new SparseMatrix(this.row, this.column);
         resultTranspose.setSparseLength(this.sparseArr.length);
         for(int i= 0; i< this.sparseArr.length; i++) {
@@ -33,10 +46,13 @@ public class SparseMatrix {
             resultTranspose.sparseArr[i][2] = this.sparseArr[i][2];
         }
    
-        resultTranspose.showSparseArray();
-        return resultTranspose.sparseArr;
+        //resultTranspose.showSparseArray();
+        return resultTranspose;
     }
-    
+    /**
+     * Method to add two sparse matrix
+     * @param newSparseMatrix2 object of second sparse matrix
+     */
     public void matrixAddition(SparseMatrix newSparseMatrix2) {
     	if(this.row != newSparseMatrix2.row || this.column!= newSparseMatrix2.column)
     		System.out.println("Addition not possible");
@@ -69,21 +85,65 @@ public class SparseMatrix {
     	    	addResult.insertInSparse(newSparseMatrix2.sparseArr[position2][0], newSparseMatrix2.sparseArr[position2][1], newSparseMatrix2.sparseArr[position2][2]);
     	    	position2++;
     	    }
-    	    addResult.showSparseArray();
+    	    addResult.showSparseMatrix();
     	}
     }
-    
+    /**
+     * Method for multiplication of two sparse matrix
+     * @param newSparseMatrix2 object of second sparse matrix
+     */
     public void matrixMultiplication(SparseMatrix newSparseMatrix2) {
-    	
+        if(this.column!= newSparseMatrix2.row)
+        	System.out.println("rows not equal to column. Multiply not Possible!!");
+        else {
+            SparseMatrix transposeMatrix2 = newSparseMatrix2.transposeMatrix();
+            int position1, position2;
+            SparseMatrix multiplyResult = new SparseMatrix(this.row, transposeMatrix2.row);
+            multiplyResult.setSparseLength(this.sparseArr.length+1);
+            for(position1= 0; position1< this.length;) {
+            	int currentRow = this.sparseArr[position1][0];
+            	for(position2= 0; position2< transposeMatrix2.length;) {
+            	    int currentColumn = transposeMatrix2.sparseArr[position2][0];
+            	    int temp1= position1;
+            	    int temp2= position2;
+            	    int sum= 0;
+            	    while(temp1< this.length && this.sparseArr[temp1][0]== currentRow && temp2< transposeMatrix2.length && transposeMatrix2.sparseArr[temp2][0]== currentColumn) {
+            	        if(this.sparseArr[position1][1]< transposeMatrix2.sparseArr[temp2][1])
+            	            temp1++;
+            	        else if(this.sparseArr[temp1][1]> transposeMatrix2.sparseArr[temp2][1]) 
+            	        	temp2++;
+            	        else 
+            	        	sum += this.sparseArr[temp1++][2] * transposeMatrix2.sparseArr[temp2++][2];
+            	    }
+            	    if(sum!= 0)
+            	        multiplyResult.insertInSparse(currentRow, currentColumn, sum);
+            	    while(position2< transposeMatrix2.length && transposeMatrix2.sparseArr[position2][0]== currentColumn)
+            	    	position2++;
+            	}
+            	while(position1< this.length && this.sparseArr[position1][0]== currentRow)
+            		position1++;
+            }
+            multiplyResult.showSparseMatrix();
+        }
     }
-    public boolean isSymmetric(int[][] transpose) {
+    /**
+     * Method to check sparse matrix is symmetric or not
+     * @param transpose contain object of transposed sparse matrix
+     * @return true if symmetric else return false
+     */
+    public boolean isSymmetric(SparseMatrix transpose) {
         for(int i=0; i< this.sparseArr.length; i++) {
-        	if(transpose[i][0]!=this.sparseArr[i][0] || transpose[i][1]!= this.sparseArr[i][1] || transpose[i][2]!= this.sparseArr[i][2])
+        	if(transpose.sparseArr[i][0]!=this.sparseArr[i][0] || transpose.sparseArr[i][1]!= this.sparseArr[i][1] || transpose.sparseArr[i][2]!= this.sparseArr[i][2])
         	    return false;
         }
         return true;
     }
-    
+    /**
+     * Method to take simple input array
+     * @param row contain the number of rows
+     * @param column contain the number of column
+     * @return simple array
+     */
     public int[][] getInputArray(int row, int column) {
     	int[][] arr = new int[row][column];
     	for(int i = 0; i < row; i++) {
@@ -93,6 +153,11 @@ public class SparseMatrix {
     	}
     	return arr;
     }
+    /**
+     * Method to calculate the number of non zero element in array
+     * @param arr contain the simple array
+     * @return count of non zero elements
+     */
     public int checkNonZeroElement(int[][] arr) {
         int count =0;
         for(int i = 0; i < this.row ; i++) {
@@ -104,7 +169,10 @@ public class SparseMatrix {
         }
         return count;
     }
-    public void showSparseArray() {
+    /**
+     * Method to display the sparse matrix
+     */
+    public void showSparseMatrix() {
     	System.out.println("values in sparse array");
     	for(int i= 0; i< sparseArr.length; i++) {
     	    System.out.print(this.sparseArr[i][0] + " " + this.sparseArr[i][1] + " " + this.sparseArr[i][2]);
@@ -113,9 +181,11 @@ public class SparseMatrix {
     }
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
+        System.out.println("enter the number of rows and column:- ");
         int row = scanner.nextInt();
         int column = scanner.nextInt();
         SparseMatrix newSparseMatrix1 = new SparseMatrix(row, column);
+        System.out.println("enter element of first array:- ");
         int[][] arr1 = newSparseMatrix1.getInputArray(row, column);
         int count = newSparseMatrix1.checkNonZeroElement(arr1);
         newSparseMatrix1.setSparseLength(count);
@@ -125,13 +195,15 @@ public class SparseMatrix {
             	    newSparseMatrix1.insertInSparse(i, j, arr1[i][j]);
             }
         }
-        newSparseMatrix1.showSparseArray();
+        newSparseMatrix1.showSparseMatrix();
         System.out.println("Transpose of a matrix:- ");
-        int[][] transposeResult= newSparseMatrix1.transposeArray();
-        boolean symmetricResult = newSparseMatrix1.isSymmetric(transposeResult);
+        SparseMatrix resultTranspose= newSparseMatrix1.transposeMatrix();
+        resultTranspose.showSparseMatrix();
+        boolean symmetricResult = newSparseMatrix1.isSymmetric(resultTranspose);
         System.out.println("is sparse matrix symmetric:- "+ symmetricResult);
         
         SparseMatrix newSparseMatrix2 = new SparseMatrix(row, column);
+        System.out.println("enter element of second array");
         int[][] arr2 = newSparseMatrix2.getInputArray(row, column);
         int count2 = newSparseMatrix1.checkNonZeroElement(arr2);
         newSparseMatrix2.setSparseLength(count2);
@@ -145,7 +217,7 @@ public class SparseMatrix {
         newSparseMatrix1.matrixAddition(newSparseMatrix2);
         
         System.out.println("Multiplication of matrix is:- ");
-        newSparseMatrix1.matrixAddition(newSparseMatrix2);
+        newSparseMatrix1.matrixMultiplication(newSparseMatrix2);
     }
 
 }
