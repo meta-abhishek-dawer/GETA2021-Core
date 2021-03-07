35ab8866ca42955626726722afef5a71156a7a53
meta-abhishek-dawer <abhishek.dawer@metacube.com>
2021-03-07 09:53:02 +0530

    Adding problem2 in which we perform operations of queue using array

---
+++ GET 2021/src/DSA_Assignment2/QueueInterface.java
@@ -0,0 +1,8 @@
+package DSA_Assignment2;
+
+public interface QueueInterface {
+    public void enQueue(int value);
+    public int deQueue();
+    public boolean isEmpty();
+    public boolean isFull();
+}
