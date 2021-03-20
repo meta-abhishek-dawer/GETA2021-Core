683c2fbfa30a2949635fa40eb45029f4a3633457
meta-abhishek-dawer <abhishek.dawer@metacube.com>
2021-03-20 16:16:39 +0530

    Adding problem2 with specification

---
+++ GET 2021/src/AlgorithmAnalysis1/Employee.java
@@ -0,0 +1,59 @@
+package AlgorithmAnalysis1;
+
+/**
+ * Class Employee having entities like id, name, age, salary of person
+ * @author abhishek.dawer_metac
+ *
+ */
+public class Employee {
+    int id, age, salary;
+    String name;
+    Employee next;
+    
+    /**
+     * Constructor of class Employee for setting values
+     * @param id containing id of employee
+     * @param name containing name of employee
+     * @param age containing age of employee
+     * @param salary containing salary of employee
+     */
+    public Employee(int id, String name, int age, int salary) {
+    	this.id = id;
+    	this.name = name;
+    	this.age = age;
+    	this.salary = salary;
+    	this.next = null;
+    }
+    
+    /**
+     * Method to get id
+     * @return id value of type int
+     */
+    public int getId() {
+    	return this.id;
+    }
+    
+    /**
+     * Method to get name
+     * @return value of type string
+     */
+    public String getName() {
+    	return this.name;
+    }
+    
+    /**
+     * Method to get age
+     * @return value of type int
+     */
+    public int getAge() {
+    	return this.age;
+    }
+    
+    /**
+     * Method to get salary
+     * @return value of type int
+     */
+    public int getSalary() {
+    	return this.salary;
+    }
+}
