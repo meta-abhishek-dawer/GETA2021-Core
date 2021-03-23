4617c6740e63d3a4d1a7f97b6917d2b97c6fbe6e
meta-abhishek-dawer <abhishek.dawer@metacube.com>
2021-03-23 19:01:06 +0530

    changes case sensitive part as per reviews

--- GET 2021/Assignment2/SQL_Assignment1.sql
+++ GET 2021/Assignment2/SQL_Assignment1.sql
@@ -1,53 +1,18 @@
-Query 1:- Create all tables
+#Query 1:- Create all tables
 
-create table Products(Product_id int, Product_Name varchar(20), InStock int(10));
-create table Category(categoryId int PRIMARY KEY, CategoryName varchar(20), CategoryType varchar(20));
-create table SubCategory(SubCategoryId int PRIMARY KEY, CategoryId int, SubCategoryName varchar(20));
-create table ItemOrder(OrderId int NOT NULL, Product_id int NOT NULL, OrderDate DATE, price int );
-create table ItemOrderStatus(OrderId int, OrderStatus varchar(15));
-create table User(UserId int PRIMARY KEY, UserName varchar(20), OrderDate DATE, OrderId int NOT NULL);
+CREATE TABLE Products(Product_id INT, Product_Name VARCHAR(20), InStock INT(10));
+CREATE TABLE Category(categoryId INT PRIMARY KEY, CategoryName VARCHAR(20), CategoryType VARCHAR(20));
+CREATE TABLE SubCategory(SubCategoryId INT PRIMARY KEY, CategoryId INT, SubCategoryName VARCHAR(20));
+CREATE TABLE ItemOrder(OrderId INT NOT NULL, Product_id INT NOT NULL, OrderDate DATE, price INT );
+CREATE TABLE ItemOrderStatus(OrderId INT, OrderStatus VARCHAR(15));
+CREATE TABLE User(UserId INT PRIMARY KEY, UserName VARCHAR(20), Email VARCHAR(20), MobileNo VARCHAR(10));
+CREATE TABLE Images(ImageId INT PRIMARY KEY, Product_id INT, Image BLOB);
 
-Query 2:- show all tables
-show tables;
+#Query 2:- show all tables
+SHOW tables;
 
-Query 3:- Delete table product
-drop table products;
+#Query 3:- Delete table product
+DROP TABLE products;
 
-Query 4:- Create product table again
-create table products(Product_id int PRIMARY KEY, ProductName varchar(20), Price int, CategoryId int, Stock int, Description varchar(20));
-
-Assginment 2
-Query1 :- Insert values in tables
-insert into products values(1, 'Íphone', 50000, 1, 10, 'Iphone11 pro');
-insert into products values(2, 'Samsung', 20000, 1, 15, 'A31s');
-insert into products values(3, 'Redmi', 15000, 1, 40, 'Note9 pro');
-insert into products values(4, 'HP', 60000, 2, 20, 'pavillion');
-insert into products values(5, 'DELL', 55000, 2, 15, 'Latitude 3410');
-insert into products values(6, 'Shirt', 600, 3, 55, 'formal');
-insert into products values(7, 'T-Shirt', 400, 3, 60, 'Half-Slieve');
-insert into products values(8, 'Pants', 800, 3, 25, 'jeans');
-insert into products values(9, 'pen', 10, 4, 100, 'Stationary');
-insert into products values(10, 'NoteBook', 30, 4, 70, 'Stationary');
-insert into products values(11, 'pencil', 5, 4, 150, 'Stationary');
-select * from products
-
-select * from products where stock>50;
-select * from category;
-insert into category values(1, 'Mobile', 'Electronics');
-insert into category values(2, 'Laptop', 'Electronics')	1 row(s);
-SET SQL_SAFE_UPDATES=0;
-delete from products where categoryId = '3';
-select * from products;
-insert into products values(6, 'Pants', 800, 3, 30, 'jeans');
-insert into products values(7, 'Pants', 1000, 3, 25, 'formal');
-insert into products values(8, 'Pants', 500, 3, 60, 'shorts');
-select * from products;
-select * from category;
-insert into category values(3, 'Pants', 'Clothes');
-select * from products;
-insert into products values(9, 'pen', 10, 4, 50, 'ball pen');
-select * from products;
-insert into products values(10, 'pen', 20, 4, 60, 'gel pen');
-insert into products values(11, 'flair', 20, 4, 20, 'writo meter');
-select * from category;
-insert into category values(4, 'Pen', 'Stationary');
+#Query 4:- Create product table again
+CREATE TABLE products(Product_id INT PRIMARY KEY, ProductName VARCHAR(20), Price INT, CategoryId INT, Stock INT, Description VARCHAR(20));
--- GET 2021/Assignment2/SQL_Assignment3.sql
+++ GET 2021/Assignment2/SQL_Assignment3.sql
@@ -1,20 +1,20 @@
-Query1:- 
-select * from itemorder ORDER BY OrderDate DESC limit 50;
+#Query1:- 
+SELECT * FROM itemorder ORDER BY OrderDate DESC LIMIT 50;
 
-Query 2:-
-select * from itemorder ORDER BY price DESC limit 10;
+#Query 2:-
+SELECT * FROM itemorder ORDER BY price DESC LIMIT 10;
 
-Query3:-
-select * from itemorder LEFT JOIN itemorderstatus ON(itemorder.OrderId = itemorderstatus.OrderId) where itemorder.OrderDate < now() - INTERVAL 10 day AND itemorderstatus.OrderStatus = 'Placed';
+#Query3:-
+SELECT * FROM itemorder LEFT JOIN itemorderstatus ON(itemorder.OrderId = itemorderstatus.OrderId) WHERE itemorder.OrderDate < now() - INTERVAL 10 day AND itemorderstatus.OrderStatus = 'Placed';
 
-Query4:-
-select user.userId, user.UserName, user.Email, user.MobileNo from user LEFT JOIN itemorder ON (user.UserId = itemOrder.UserId) BETWEEN '2021-02-16' AND '2021-03-15';
+#Query4:-
+SELECT user.userId, user.UserName, user.Email, user.MobileNo FROM user LEFT JOIN itemorder ON (user.UserId = itemOrder.UserId) BETWEEN '2021-02-16' AND '2021-03-15';
 
-Query5:-
-select * from user INNER JOIN itemorder ON(user.UserId = itemorder.UserId) where OrderDate > now() - INTERVAL 15 day;
+#Query5:-
+SELECT * FROM user INNER JOIN itemorder ON(user.UserId = itemorder.UserId) WHERE OrderDate > now() - INTERVAL 15 day;
 
-Query6:-
-select * from itemorder LEFT JOIN itemorderstatus ON(itemorder.OrderId = itemorderstatus.OrderId) where itemorderstatus.OrderId = 2 AND itemorderstatus.OrderStatus = 'Shipped';
+#Query6:-
+SELECT * FROM itemorder LEFT JOIN itemorderstatus ON(itemorder.OrderId = itemorderstatus.OrderId) WHERE itemorderstatus.OrderId = 2 AND itemorderstatus.OrderStatus = 'Shipped';
 
-Query7:-
-select * from itemorder where Price BETWEEN 20 AND 50;
+#Query7:-
+SELECT * FROM itemorder WHERE Price BETWEEN 20 AND 50;
